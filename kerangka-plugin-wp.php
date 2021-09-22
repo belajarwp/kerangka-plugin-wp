5d92e3624b39f26fe13747c30da2b14a33c2ab4d
Hadie Danker <hadie87@gmail.com>
2021-09-22 09:37:56 +0700

    first commit

---
+++ kerangka-plugin-wp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Kerangka Plugin WordPress
+ * Plugin URI:        https://dankedev.com/kelas/master-plugin-wordpress
+ * Description:       Belajar cara membuat plugin WordPress untuk kerangka plugin
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Hadie Danker
+ * Author URI:        https://facebook.com/hadie.danker
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       kerangka-plugin-wp
+ * Domain Path:       /languages
+ */
+
+ 
